8952e870ea6ae8065aa59dd3835baa78c157f1a8
Mic <micf174@gmail.com>
2021-05-04 14:11:14 +0200

    Création de la classe Obstacle.kt

--- app/src/main/java/com/example/curlinggame/Obstacle.kt
+++ app/src/main/java/com/example/curlinggame/Obstacle.kt
@@ -1,21 +1,36 @@
 package com.example.curlinggame
 
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
 import java.util.*
 
 
-open class Obstacle (){
+open class Obstacle () {
     val random = Random()
 
-    class ObstacleC(){
+    class Obstaclec(var x1: Float, var y1: Float, var rayonO1: Float) {
+        val ObstaclecPaint = Paint()
+        val color1 = Color.GRAY
+        val r1 = RectF(x1, y1, x1 + (2 * rayonO1), y1 - (2 * rayonO1))
 
+        fun draw(canvas: Canvas?) {
+            ObstaclecPaint.color = color1
+            if (canvas != null) {
+                canvas.drawRect(r1, ObstaclecPaint)
+            }
+        }
     }
 
-    class ObstacleR(){
+    class Obstacler(var x1: Float, var y1: Float, var rayonO2: Float) {
+        val ObstaclerPaint = Paint()
+        val color2 = Color.RED
+        val r2 = RectF(x1, y1, x1 + (2 * rayonO2), y1 - (2 * rayonO2))
 
-    }
-
-    fun draw(canvas: Canvas?) {
-
-    }
+        fun draw(canvas: Canvas?) {
+            ObstaclerPaint.color = color2
+            canvas?.drawOval(r2, ObstaclerPaint)
+        }
+        }
 }
--- app/src/main/java/com/example/curlinggame/Player.kt
+++ app/src/main/java/com/example/curlinggame/Player.kt
@@ -2,6 +2,7 @@ package com.example.curlinggame
 import CurlingView
 import android.graphics.Paint
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.RectF
 
 
@@ -10,8 +11,10 @@ class Player (var lsquareP: Float, var lsquareLP : Float, val view : CurlingView
     val playerPaint = Paint()
     val r = RectF(view.width / 2 - 10f, lsquareP, view.width / 2 + 10f, 0f)
     val r1 = RectF(view.width / 2 - 5f, lsquareP + lsquareLP, view.width / 2 + 5f, lsquareP)
+    val color = Color.BLACK
 
     fun draw(canvas: Canvas) {
+        playerPaint.color = color
         canvas.drawRect(r, playerPaint)
         canvas.drawRect(r1, playerPaint)
     }
