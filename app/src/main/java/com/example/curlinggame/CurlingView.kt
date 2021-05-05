232477bb67774aab0b4fec6ddc496370eaa83dcc
Mic <micf174@gmail.com>
2021-05-05 16:05:11 +0200

    Dimensionnement des objets + Création de fonction utile

--- app/src/main/java/com/example/curlinggame/CurlingView.kt
+++ app/src/main/java/com/example/curlinggame/CurlingView.kt
@@ -6,22 +6,19 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.SurfaceHolder
 import android.view.SurfaceView
-import java.util.*
-class CurlingView @JvmOverloads constructor (context: Context, attributes: AttributeSet? = null, defStyleAttr: Int = 0): SurfaceView(context, attributes,defStyleAttr), SurfaceHolder.Callback, Runnable{
 
-    lateinit var canvas : Canvas
-
-    lateinit var thread = Thread
-
+class CurlingView @JvmOverloads constructor (context: Context, attributes: AttributeSet? = null, defStyleAttr: Int = 0): SurfaceView(context, attributes,defStyleAttr), SurfaceHolder.Callback, Runnable {
+    lateinit var canvas: Canvas
+    val FD = Paint()
     var width = 0f
     var height = 0f
     var drawing = false
-    val FD = Paint()
-    val player = Player(0f ,0f , this)
-    val obstacle1 = Obstacle.Obstaclec(0f,0f,0f)
-    val obstacle2 = Obstacle.Obstacler(0f,0f,0f)
-    val cible = Cible(0f,0f,0f)
-    val pave = Pave(0f,0f,0f)
+    lateinit var thread = Thread
+    val player = Player(0f, 0f, this)
+    val obstacle1 = Obstacle.Obstaclec(0f, 0f, 0f)
+    val obstacle2 = Obstacle.Obstacler(0f, 0f, 0f)
+    val cible = Cible(0f, 0f, 0f)
+    val pave = Pave(0f, 0f, 0f)
 
     init {
         FD.color = Color.GREEN
@@ -49,41 +46,50 @@ class CurlingView @JvmOverloads constructor (context: Context, attributes: Attri
         width = w.toFloat()
         height = h.toFloat()
 
-        player.lsquareP = ()
-        player.lsquareLP =()
+        player.lsquareP = (w/24f)
+        player.lsquareLP = (w/48f)
 
-        pave.x1 = ()
-        pave.y1 = ()
-        pave.rayonP = ()
+        pave.x1 = (w/2f)
+        pave.y1 = (0f)
+        pave.rayonP = (w/64f)
 
-        obstacle1.x1 = ()
-        obstacle1.y1 = ()
-        obstacle1.rayonO1 = ()
+        obstacle1.x1 = (w/50f)
+        obstacle1.y1 = (w/50f)
+        obstacle1.rayonO1 = (w/100f)
 
-        obstacle2.x1 = ()
-        obstacle2.y1 = ()
-        obstacle2.rayonO2 = ()
+        obstacle2.x1 = (w/50f)
+        obstacle2.y1 = (w/50f)
+        obstacle2.rayonO2 = (w/100f)
 
-        cible.x1 = ()
-        cible.y1 = ()
-        cible.rayonC = ()
-
-
+        cible.x1 = (w/100f)
+        cible.y1 = (w/200f)
+        cible.rayonC = (w/50f)
     }
 
-    override fun onTouchEvent(){
-
+    fun draw() {
+        if (holder.surface.isValid) {
+            canvas = holder.lockCanvas()
+            canvas.drawRect(0f, 0f, canvas.width.toFloat(),
+                    canvas.height.toFloat(), FD)
+            player.draw(canvas)
+            pave.draw(canvas)
+            obstacle1.draw(canvas)
+            obstacle2.draw(canvas)
+            cible.draw(canvas)
+            holder.unlockCanvasAndPost(canvas)
+        }
     }
 
-    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
 
+
+        override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+
+        }
+
+        override fun surfaceCreated(holder: SurfaceHolder) {
+
+        }
+
+        override fun surfaceDestroyed(holder: SurfaceHolder) {
+        }
     }
-
-    override fun surfaceCreated(holder: SurfaceHolder) {
-
-    }
-
-    override fun surfaceDestroyed(holder: SurfaceHolder) {
-    }
-}
-
