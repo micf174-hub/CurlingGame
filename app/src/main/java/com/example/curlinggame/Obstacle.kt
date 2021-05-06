6797a2eb7576f6405a0a8183626a338752b16a9e
Lulu <LuciePaucar>
2021-05-06 18:00:50 +0200

    position obsatcle

--- app/src/main/java/com/example/curlinggame/Obstacle.kt
+++ app/src/main/java/com/example/curlinggame/Obstacle.kt
@@ -7,10 +7,10 @@ import android.graphics.RectF
 
 class Obstacle () {
 
-    class Obstaclec(var x1: Float, var y1: Float, var rayonO1: Float) {
+    class Obstaclec( var rayonO1: Float, val view: CurlingView) {
         val ObstaclecPaint = Paint()
         val color1 = Color.GRAY
-        val r1 = RectF(x1, y1, x1 + (2 * rayonO1), y1 - (2 * rayonO1))
+        val r1 = RectF(view.width/3, 2*view.height/3 , view.width/3 + rayonO1, 2*view.height/3 - rayonO1)
 
         fun draw(canvas: Canvas?) {
             ObstaclecPaint.color = color1
@@ -19,14 +19,26 @@ class Obstacle () {
         }
     }
 
-    class Obstacler(var x1: Float, var y1: Float, var rayonO2: Float) {
+    class Obstacler( var rayonO2: Float, val view: CurlingView) {
         val ObstaclerPaint = Paint()
         val color2 = Color.RED
-        val r2 = RectF(x1, y1, x1 + (2 * rayonO2), y1 - (2 * rayonO2))
+        val r2 = RectF(0f, view.height/2, rayonO2, view.height/2 - rayonO2)
 
         fun draw(canvas: Canvas?) {
             ObstaclerPaint.color = color2
             canvas?.drawOval(r2, ObstaclerPaint)
         }
     }
+
+    class Obstacleg(var rayonO3: Float, val view: CurlingView) {
+        val ObstaclerPaint = Paint()
+        val color3 = Color.BLUE
+        val r3 = RectF(x1, y1, x1 + (2 * rayonO2), y1 - (2 * rayonO2))
+
+        fun draw(canvas: Canvas?) {
+            ObstaclerPaint.color = color3
+            canvas?.drawOval(r3, ObstaclerPaint)
+        }
+    }
+
 }
