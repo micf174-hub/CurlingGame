8e341e0ab81f7fdf60b495a0f8873b52c78eeb81
Mic <micf174@gmail.com>
2021-05-08 19:56:25 +0200

    Ajout

--- app/src/main/java/com/example/curlinggame/Player.kt
+++ app/src/main/java/com/example/curlinggame/Player.kt
@@ -9,8 +9,8 @@ import android.graphics.RectF
 class Player (var lsquareP: Float, var lsquareLP : Float, var view : CurlingView) {
 
     val playerPaint = Paint()
-    val r = RectF(view.width / 2, lsquareP, view.width, 0f)
-    val r1 = RectF(view.width / 2 , lsquareP + lsquareLP, view.width, lsquareP)
+    val r = RectF(view.width , lsquareP, view.width/2, 0f)
+    val r1 = RectF(view.width  , lsquareP + lsquareLP, view.width/2, lsquareP)
     val color = Color.BLACK
 
     fun draw(canvas: Canvas?) {
