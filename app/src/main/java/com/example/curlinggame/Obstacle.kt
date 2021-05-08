0ee53f3e0c6c9f715c60cb15ae142a6ff7fe5a23
Lulu <LuciePaucar>
2021-05-08 13:56:37 +0200

    override de la méthode interactions avec le pave pour les 3 types d'obstacles

--- app/src/main/java/com/example/curlinggame/Obstacle.kt
+++ app/src/main/java/com/example/curlinggame/Obstacle.kt
@@ -18,7 +18,7 @@ open class Obstacle (val lepave: Pave) {
         var r1 = RectF(view.width/3, 2*view.height/3 , view.width/3 + rayonO1, 2*view.height/3 - rayonO1)
 
         override fun interaction() {
-            lepave.Bouge()
+            lepave.Rebondit()
         }
         fun draw(canvas: Canvas?) {
             ObstaclecPaint.color = color1
@@ -33,6 +33,9 @@ open class Obstacle (val lepave: Pave) {
         val color2 = Color.RED
         var r2 = RectF(2*view.width/3, 2*view.height/3, 2*view.width/3 + rayonO2, 2*view.height/3 - rayonO2)
 
+        override fun interaction() {
+            lepave.Disparait()
+        }
         fun draw(canvas: Canvas?) {
             ObstaclerPaint.color = color2
             canvas?.drawOval(r2, ObstaclerPaint)
@@ -44,6 +47,9 @@ open class Obstacle (val lepave: Pave) {
         val color3 = Color.BLUE
         val r3 = RectF(view.width /2, 2*view.height/3, view.width/2 + rayonO3, 2*view.height/3 + rayonO3)
 
+        override fun interaction() {
+            lepave.Apparait()
+        }
         fun draw(canvas: Canvas?) {
             ObstaclerPaint.color = color3
             canvas?.drawOval(r3, ObstaclerPaint)
