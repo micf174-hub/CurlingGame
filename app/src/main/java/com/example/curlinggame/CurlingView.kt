065c9c9750e1d243d91b117d6706fe478505e96f
Mic <micf174@gmail.com>
2021-05-08 17:19:15 +0200

    Mini modif

--- app/src/main/java/com/example/curlinggame/CurlingView.kt
+++ app/src/main/java/com/example/curlinggame/CurlingView.kt
@@ -47,8 +47,8 @@ class CurlingView @JvmOverloads constructor (context: Context, attributes: Attri
         width = w.toFloat()
         height = h.toFloat()
 
-        player.lsquareP = (w/24f)
-        player.lsquareLP = (w/48f)
+        player.lsquareP = (w/2f)
+        player.lsquareLP = (w/5f)
 
         pave.x1 = (w/2f)
         pave.y1 = (0f)
@@ -71,13 +71,8 @@ class CurlingView @JvmOverloads constructor (context: Context, attributes: Attri
             canvas = holder.lockCanvas()
             canvas.drawRect(0f, 0f, canvas.width.toFloat(),
                     canvas.height.toFloat(), FD)
-            player.draw(canvas)
-            if (paveE)
-                pave.draw(canvas)
-                obstacle1.draw(canvas)
-                obstacle2.draw(canvas)
-                cible.draw(canvas)
-                holder.unlockCanvasAndPost(canvas)
+            cible.draw(canvas)
+            holder.unlockCanvasAndPost(canvas)
         }
     }
         override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
