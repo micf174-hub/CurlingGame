d210399797a33a34f3bebf468ce6273c43d1bdb8
Mic <micf174@gmail.com>
2021-05-09 03:28:52 +0200

    Amélio

--- app/src/main/java/com/example/curlinggame/Cible.kt
+++ app/src/main/java/com/example/curlinggame/Cible.kt
@@ -9,35 +9,32 @@ import java.util.*
 class Cible (var rayonC : Float, val  view: CurlingView) {
     val random = Random()
     val ciblePaint = Paint()
-    val rand1 = random.nextFloat() * view.width
-    val rand2 = random.nextFloat() * view.height
+    val textPaint = Paint()
+    var NB_C = 10
+    var rand1 = random.nextFloat() * view.width
+    var rand2 = random.nextFloat() * view.height
     var r4 = RectF( rand1 - rayonC,rand2-rayonC, rand1 + rayonC, rand2 + rayonC)
 
     fun rand(from: Int, to: Int): Int {
         return random.nextInt(to - from) + from
     }
     fun draw(canvas: Canvas) {
-        val NB_R = rand(0, 2)
-        var NB_C = 0
-        val rand3 = rand1
-        val rand4 = rand2
-        var r5 = RectF( rand3 - rayonC,rand4-rayonC, rand3 + rayonC, rand4 + rayonC)
-        while (NB_C < 10) {
-            NB_C++
+        val NB_R = rand(0, 3)
+        for (i in 0 until NB_C) {
             if (NB_R == 0) {
                 ciblePaint.color = Color.YELLOW
-                canvas.drawRect(r5, ciblePaint)
-                canvas.drawText("5", rand3 , rand4, ciblePaint)
+                canvas.drawRect(r4, ciblePaint)
+                canvas.drawText("5", rand1 , rand2, textPaint)
             }
             else if (NB_R == 1) {
                 ciblePaint.color = Color.RED
-                canvas.drawRect(r5, ciblePaint)
-                canvas.drawText("10", rand3, rand4, ciblePaint)
+                canvas.drawRect(r4, ciblePaint)
+                canvas.drawText("10", rand1, rand2, textPaint)
             }
             else if (NB_R == 2) {
                 ciblePaint.color = Color.BLUE
-                canvas.drawOval(r5, ciblePaint)
-                canvas.drawText("10", rand3, rand4, ciblePaint)
+                canvas.drawOval(r4, ciblePaint)
+                canvas.drawText("10", rand1, rand2, textPaint)
             }
         }
     }
--- app/src/main/java/com/example/curlinggame/Pave.kt
+++ app/src/main/java/com/example/curlinggame/Pave.kt
@@ -7,9 +7,8 @@ class Pave (var rayonP : Float, var hauteur : Float, val view : CurlingView) {
     val random = Random()
     val pavePaint = Paint()
     val r = RectF(view.width/2-10f, hauteur, view.width/2+10f,0f)
-    val color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
     fun draw(canvas: Canvas?) {
-        pavePaint.color = color
+        pavePaint.color = Color.RED
         canvas?.drawOval(r, pavePaint)
     }
 
