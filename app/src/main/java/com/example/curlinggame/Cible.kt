4d8056b0ac411950fa6212db63ca4ff6bde7fdce
Mic <micf174@gmail.com>
2021-05-10 01:36:48 +0200

    Classe Cible

--- app/src/main/java/com/example/curlinggame/Cible.kt
+++ app/src/main/java/com/example/curlinggame/Cible.kt
@@ -3,14 +3,14 @@ package com.example.curlinggame
 import android.graphics.*
 import java.util.*
 
-open class Cible (var hauteur1 : Float, var largeur1: Float ,var rayonC : Float,var interval : Float, val  view: CurlingView) {
+open class Cible (var hauteur1 : Float, var largeur1: Float ,var rayonC : Float,var intervalC : Float, val  view: CurlingView) {
     val cible1Paint = Paint()
     val cible2Paint = Paint()
 
     fun draw(canvas: Canvas) {
         for (i in 0..10) {
-            val r1 = RectF(largeur1 + (2 * i * interval), hauteur1, largeur1 + rayonC + (2 * i * interval), hauteur1 + rayonC)
-            val r2 = RectF(largeur1 + (2 * i * interval), hauteur1 + 2 * interval, largeur1 + rayonC + (2 * i * interval),hauteur1 + 2 * interval + rayonC)
+            val r1 = RectF(largeur1 + (2 * i * intervalC), hauteur1, largeur1 + rayonC + (2 * i * intervalC), hauteur1 + rayonC)
+            val r2 = RectF(largeur1 + (2 * i * intervalC), hauteur1 + 2 * intervalC, largeur1 + rayonC + (2 * i * intervalC),hauteur1 + 2 * intervalC + rayonC)
             if (i  % 2 == 0) {
                 cible1Paint.color = Color.YELLOW
                 cible2Paint.color = Color.WHITE
