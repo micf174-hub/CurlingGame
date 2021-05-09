70b416a66cb14903616567d18a75745ec0c302c2
Lulu <LuciePaucar>
2021-05-09 11:46:12 +0200

    Appel obstacle correct aussi dans Pave.kt

--- app/src/main/java/com/example/curlinggame/Pave.kt
+++ app/src/main/java/com/example/curlinggame/Pave.kt
@@ -8,9 +8,9 @@ open class Pave (var x1 : Float, var y1 : Float, var rayonP : Float, val view : 
     val pavePaint = Paint()
     val r = RectF(x1, y1, x1 + (2 * rayonP), y1 - (2 * rayonP))
     val color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
-    var t1 = Obstacle.Obstaclec().r1
-    var t2 = Obstacle.Obstacler().r2
-    var t3 = Obstacle.Obstacleg().r3
+    var t1 = Obstaclec().r1
+    var t2 = Obstacler().r2
+    var t3 = Obstacleg().r3
     var dx: Int
     var dy: Int
     var canonpaveVitesseX = 0f
