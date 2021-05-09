73ed25bcd4b26305962688cd5180cafa228e4a00
Lulu <LuciePaucar>
2021-05-09 18:10:06 +0200

    fun align pour l'angle

--- app/src/main/java/com/example/curlinggame/Player.kt
+++ app/src/main/java/com/example/curlinggame/Player.kt
@@ -1,23 +1,26 @@
 package com.example.curlinggame
 
-import android.graphics.Paint
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.RectF
+import android.graphics.*
 
 
-class Player (var hauteur1: Float, var hauteur2 : Float, val view : CurlingView) {
+class Player (var hauteur1: Float, var hauteur2 : Float, var hauteur : Float, val view : CurlingView) {
 
     val playerPaint = Paint()
     val playerPaint1 = Paint()
     val r = RectF(view.width / 2 - view.width / 10  , hauteur1, view.width / 2 + view.width / 10, view.height)
     val r1 = RectF(view.width / 2 - view.width / 20,hauteur2,view.width/2 + view.width / 20,hauteur1)
+    var finCanon = PointF(hauteur1, hauteur2)
 
     fun draw(canvas: Canvas) {
         playerPaint.color = Color.BLACK
         playerPaint1.color = Color.BLACK
         canvas.drawRect(r, playerPaint)
         canvas.drawRect(r1, playerPaint1)
+    }
+    fun align(angle: Double) {
+        finCanon.x = (hauteur2 * Math.sin(angle)).toFloat()
+        finCanon.y = (-hauteur2 * Math.cos(angle)
+                + view.height / 2).toFloat()
     }
 
     fun setRect() {
