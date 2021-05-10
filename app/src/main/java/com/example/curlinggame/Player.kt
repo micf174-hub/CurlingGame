16bfb4697200657a35a7f154eb4638907db27a3f
Mic <micf174@gmail.com>
2021-05-10 11:30:10 +0200

    Canon finaliser

--- app/src/main/java/com/example/curlinggame/Player.kt
+++ app/src/main/java/com/example/curlinggame/Player.kt
@@ -3,17 +3,17 @@ package com.example.curlinggame
 import android.graphics.*
 
 
-class Player (var hauteur1: Float, var hauteur2 : Float, var epaisseur : Float, val view : CurlingView) {
+class Player (var hauteur1: Float, var hauteur2 : Float, var largeur : Float, var epaisseur : Float, val view : CurlingView) {
 
     val playerPaint = Paint()
     val r = RectF(view.width / 2 - view.width / 10  , hauteur1, view.width / 2 + view.width / 10, view.height)
-    var r1 = PointF(view.width/2 +10f, hauteur1)
+    var r1 = PointF(largeur, hauteur2)
 
     fun draw(canvas: Canvas) {
         playerPaint.color = Color.BLACK
         canvas.drawRect(r, playerPaint)
         playerPaint.strokeWidth = epaisseur * 1.5f
-        canvas.drawLine(view.width/2 - 10f , hauteur2, r1.x,
+        canvas.drawLine(view.width/2 , hauteur1, r1.x,
                 r1.y, playerPaint)
     }
     fun setRect() {
@@ -21,11 +21,11 @@ class Player (var hauteur1: Float, var hauteur2 : Float, var epaisseur : Float, 
     }
 
     fun setr1() {
-        r1.set(view.width/2 + 10f, hauteur1)
+        r1.set(largeur, hauteur2)
     }
 
     fun alignement(angle: Double) {
-        r1.x = (view.width/2 + (hauteur1-hauteur2) * Math.sin(angle)).toFloat()
-        r1.y = ((hauteur1-hauteur2) * Math.cos(angle)).toFloat()
+        r1.x = (view.width/2 + hauteur2 * Math.sin(angle)).toFloat()
+        r1.y = ((hauteur2) * Math.cos(angle)).toFloat()
     }
 }
